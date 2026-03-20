cdc42131f123cbb4cac24baeaa07f3796d03b9a1
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-03-20 10:29:01 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public class Test
+{
+    public int Mid;
+    public int Final,
+}
+public double Getavg()
+{
+    return (Mid + Final) / 2.0;
+}
+
+public void SetGrade(int m, int f)
+{
+    Mid = m;
+    Final = f;
+
+    
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace class_3_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Test joe = new Test();
+           Test joe = new Test();
+            Test tom;
+
+
+            //joe.Mid = 80;
+            //joe.Final = 76;
+            joe.SetGrade(80, 76);
+
+            tom = joe;
+
+            // marry.Mid = 65;
+            // marry.Mid = 80;
+
+            // Console.WriteLine("joe" + joe.Mid);
+            // Console.WriteLine("joe" + joe.Final);
+
+            // Console.WriteLine("marry" + marry.Final);
+            //Console.WriteLine("marry" + marry.Final);
+
+            //joe.Mid = 59;
+            // joe.Final = 40;
+
+            //Console.WriteLine("tom" + tom.Mid);
+            //Console.WriteLine("tom+ tom.Final);
+
+
+
+
+
+
+        }
+    }
+}
